7f1105ed4244eabc11017f8d2ae289ab8fe64360
nicole tran <nicolet2090@gmail.com>
2023-07-18 13:57:14 -0700

    Initial Commit

---
+++ advice/adviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  adviceApp.swift
+//  advice
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct adviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
